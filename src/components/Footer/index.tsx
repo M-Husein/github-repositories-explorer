8df07a97499b5d6002321fc6931d2b0eee28bb8a
M-Husein <m.husein27@gmail.com>
2023-06-17 15:55:00 +0700

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
       data-testid="footerMain"
       className="p-4 bg-slate-200 bg-mode-2 text-center text-sm"
     >
-      © 2023 <a href="https://www.linkedin.com/in/muhamad-husein" target="_blank" rel="noopener noreferrer" className="no-underline">Muhamad Husein</a>.
+      © 2023 <a href="https://www.linkedin.com/in/muhamad-husein" target="_blank" rel="noopener noreferrer" className="no-underline font-medium">Muhamad Husein</a>.
       <br/>
       All Rights Reserved.
     </footer>
