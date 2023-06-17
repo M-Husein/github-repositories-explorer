8752cdfde8ba4652d3d1b562a2ac428ce1e0d918
M-Husein <m.husein27@gmail.com>
2023-06-17 11:48:46 +0700

    test again

--- src/app/repo/[user]/[reponame]/page.tsx
+++ src/app/repo/[user]/[reponame]/page.tsx
@@ -1,30 +1,30 @@
-import { Metadata } from 'next';
-// import { RepoDetail } from '@/components/RepoDetail';
-import { APP_NAME } from '@/const/APPS';
+// import { Metadata } from 'next';
+import { RepoDetail } from '@/components/RepoDetail';
+// import { APP_NAME } from '@/const/APPS';
 
-type metaProps = {
+type pageProps = {
   params: { user: string, reponame: string }
 }
 
-export async function generateMetadata(
-  { params }: metaProps,
-): Promise<Metadata> {
-  const title = params.user + '/' + params.reponame;
-  return {
-    title,
-    openGraph: {
-      title: title + ' | ' + APP_NAME,
-    },
-    twitter: {
-      title: title + ' | ' + APP_NAME,
-    },
-  }
-}
+// export async function generateMetadata(
+//   { params }: pageProps,
+// ): Promise<Metadata> {
+//   const title = params.user + '/' + params.reponame;
+//   return {
+//     title,
+//     openGraph: {
+//       title: title + ' | ' + APP_NAME,
+//     },
+//     twitter: {
+//       title: title + ' | ' + APP_NAME,
+//     },
+//   }
+// }
 
-export default function Detail({ params }: metaProps){
+export default function Detail({ params }: pageProps){
   return (
     <div className="w-full max-w-5xl mx-auto p-4 flex flex-col">
-      {/* <RepoDetail {...params} /> */}
+      <RepoDetail {...params} />
     </div>
   );
 }
