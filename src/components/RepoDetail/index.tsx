81b81054675cd23e7f419290f7c3eefa6660a448
M-Husein <m.husein27@gmail.com>
2023-06-23 11:44:26 +0700

    feat: add speech content in detail page

--- src/components/RepoDetail/index.tsx
+++ src/components/RepoDetail/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
@@ -8,6 +8,7 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import { BsEye, BsStarFill, BsLink45Deg, BsDownload } from "react-icons/bs";
 import { Img } from '@/components/Img';
 import { MarkdownView } from '@/components/MarkdownView';
+import { SpeechContent } from '@/components/SpeechContent';
 import { fetchApi, numShort, parseDate } from '@/utils';
 import { useApi } from '@/components/Apps';
 
@@ -18,6 +19,7 @@ type RepoDetailProps = {
 
 export const RepoDetail = ({ user, repo }: RepoDetailProps) => {
   const api = useApi() as any;
+  const contentRef = useRef() as any;
   const [data, setData] = useState<any>({});
   const [loading, setLoading] = useState<any>(true);
   const [markdown, setMarkdown] = useState<any>('');
@@ -90,153 +92,166 @@ export const RepoDetail = ({ user, repo }: RepoDetailProps) => {
     <Spinner animation="border" className="text-blue-600 mx-auto my-4" />
     :
     <>
-      <h1 className="text-3xl break-all">
-        <a
-          href={data.owner.html_url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="no-underline font-normal"
-        >
-          <Img
-            width={32}
-            height={32}
-            draggable={false}
-            alt={user}
-            src={data.owner.avatar_url}
-            className="rounded mr-4"
-          />
-          {user}
-        </a>
-        <span className="mx-1">/</span>
-        <a
-          href={data.html_url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="no-underline"
-        >
-          {repo}
-        </a>
-        <small className="inline-block align-middle py-1 px-2 rounded-xl bg-blue-100 bg-mode text-xs ml-2">
-          {data.private ? 'Private' : 'Public'}
-        </small>
-      </h1>
+      {!loading && !!markdown && (
+        <SpeechContent
+          text={contentRef.current?.innerText || ''}
+          dropdownProps={{
+            drop: "down",
+            align: { sm: "end" }
+          }}
+          className="shadow bg-body-tertiary sticky top-14 left-0 right-0 z-30 p-1 mb-6 lg:mx-0 lg:mt-0 -mx-4 -mt-4"
+        />
+      )}
 
-      <div className="text-xs mt-2">
-        <span className="inline-block py-1 px-2 rounded bg-gray-200 bg-mode mb-1">
-          Created at: <time dateTime={data.created_at}>{parseDate(data.created_at)}</time>
-        </span>
-        <span className="inline-block py-1 px-2 rounded bg-gray-200 bg-mode mb-1 mx-1">
-          Updated at: <time dateTime={data.updated_at}>{parseDate(data.updated_at)}</time>
-        </span>
-        <span className="inline-block py-1 px-2 rounded bg-gray-200 bg-mode mb-1">
-          Pushed at: <time dateTime={data.pushed_at}>{parseDate(data.pushed_at)}</time>
-        </span>
-      </div>
+      <article ref={contentRef}>
+        <h1 className="text-3xl break-all">
+          <a
+            href={data.owner.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="no-underline font-normal"
+          >
+            <Img
+              width={32}
+              height={32}
+              draggable={false}
+              alt={user}
+              src={data.owner.avatar_url}
+              className="rounded mr-4"
+            />
+            {user}
+          </a>
+          <span className="mx-1">/</span>
+          <a
+            href={data.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="no-underline"
+          >
+            {repo}
+          </a>
+          <small className="inline-block align-middle py-1 px-2 rounded-xl bg-blue-100 bg-mode text-xs ml-2">
+            {data.private ? 'Private' : 'Public'}
+          </small>
+        </h1>
 
-      <div className="my-5 flex flex-col lg:flex-row flex-wrap gap-2">
-        <Button
-          as="div"
-          variant="outline-secondary"
-          className="flex items-center gap-x-2 cursor-auto"
-        >
-          <BsEye />
-          Watch
-          <b className="border border-solid border-gray-400 px-2 rounded-lg ml-auto">
-            {data.watchers_count ? numShort(data.watchers_count) : 0}
-          </b>
-        </Button>
-        <Button
-          as="div"
-          variant="outline-secondary"
-          className="flex items-center gap-x-2 cursor-auto"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-            <path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"/>
-          </svg>
-          Fork
-          <b className="border border-solid border-gray-400 px-2 rounded-lg ml-auto">
-            {data.forks_count ? numShort(data.forks_count) : 0}
-          </b>
-        </Button>
-        <Button
-          as="div"
-          variant="outline-secondary"
-          className="flex items-center gap-x-2 cursor-auto"
-        >
-          <BsStarFill />
-          Starred
-          <b className="border border-solid border-gray-400 px-2 rounded-lg ml-auto">
-            {data.stargazers_count ? numShort(data.stargazers_count) : 0}
-          </b>
-        </Button>
-        <Dropdown>
-          <Dropdown.Toggle variant="outline-secondary" className="w-full" id="ddCode">
-            Code
-          </Dropdown.Toggle>
-          <Dropdown.Menu className="shadow w-full lg:min-w-400px">
-            <Dropdown.Header>Clone</Dropdown.Header>
-            {
-              [
-                { label: "HTTPS", value: data.clone_url },
-                { label: "SSH", value: data.ssh_url },
-                { label: "GitHub CLI", value: 'gh repo clone ' + data.full_name },
-              ].map((item: any, idx: number) =>
-                <div key={item.label} className="py-2 px-4">
-                  <label htmlFor={"cloneInput" + idx}>{item.label}</label>
-                  <div className="input-group input-group-sm mt-1">
-                    <FormControl
-                      readOnly
-                      defaultValue={item.value}
-                      onMouseUp={selectAll}
-                      className="truncate"
-                      id={"cloneInput" + idx}
-                    />
-                    <Button onClick={copyToClipboard} variant="outline-secondary">Copy</Button>
+        <div className="text-xs mt-2">
+          <span className="inline-block py-1 px-2 rounded bg-gray-200 bg-mode mb-1">
+            Created at: <time dateTime={data.created_at}>{parseDate(data.created_at)}</time>
+          </span>
+          <span className="inline-block py-1 px-2 rounded bg-gray-200 bg-mode mb-1 mx-1">
+            Updated at: <time dateTime={data.updated_at}>{parseDate(data.updated_at)}</time>
+          </span>
+          <span className="inline-block py-1 px-2 rounded bg-gray-200 bg-mode mb-1">
+            Pushed at: <time dateTime={data.pushed_at}>{parseDate(data.pushed_at)}</time>
+          </span>
+        </div>
+
+        <div className="my-5 flex flex-col lg:flex-row flex-wrap gap-2">
+          <Button
+            as="div"
+            variant="outline-secondary"
+            className="flex items-center gap-x-2 cursor-auto"
+          >
+            <BsEye />
+            Watch
+            <b className="border border-solid border-gray-400 px-2 rounded-lg ml-auto">
+              {data.watchers_count ? numShort(data.watchers_count) : 0}
+            </b>
+          </Button>
+          <Button
+            as="div"
+            variant="outline-secondary"
+            className="flex items-center gap-x-2 cursor-auto"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
+              <path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"/>
+            </svg>
+            Fork
+            <b className="border border-solid border-gray-400 px-2 rounded-lg ml-auto">
+              {data.forks_count ? numShort(data.forks_count) : 0}
+            </b>
+          </Button>
+          <Button
+            as="div"
+            variant="outline-secondary"
+            className="flex items-center gap-x-2 cursor-auto"
+          >
+            <BsStarFill />
+            Starred
+            <b className="border border-solid border-gray-400 px-2 rounded-lg ml-auto">
+              {data.stargazers_count ? numShort(data.stargazers_count) : 0}
+            </b>
+          </Button>
+          <Dropdown>
+            <Dropdown.Toggle variant="outline-secondary" className="w-full" id="ddCode">
+              Code
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="shadow w-full lg:min-w-400px">
+              <Dropdown.Header>Clone</Dropdown.Header>
+              {
+                [
+                  { label: "HTTPS", value: data.clone_url },
+                  { label: "SSH", value: data.ssh_url },
+                  { label: "GitHub CLI", value: 'gh repo clone ' + data.full_name },
+                ].map((item: any, idx: number) =>
+                  <div key={item.label} className="py-2 px-4">
+                    <label htmlFor={"cloneInput" + idx}>{item.label}</label>
+                    <div className="input-group input-group-sm mt-1">
+                      <FormControl
+                        readOnly
+                        defaultValue={item.value}
+                        onMouseUp={selectAll}
+                        className="truncate"
+                        id={"cloneInput" + idx}
+                      />
+                      <Button onClick={copyToClipboard} variant="outline-secondary">Copy</Button>
+                    </div>
                   </div>
-                </div>
-              )
-            }
-            <hr className="my-2" />
-            <Dropdown.Item
-              href={`https://github.com/${user}/${repo}/archive/refs/heads/${data.default_branch}.zip`}
-              download
-            >
-              <BsDownload className="mr-2" />Download ZIP
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
+                )
+              }
+              <hr className="my-2" />
+              <Dropdown.Item
+                href={`https://github.com/${user}/${repo}/archive/refs/heads/${data.default_branch}.zip`}
+                download
+              >
+                <BsDownload className="mr-2" />Download ZIP
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
 
-      <hr />
+        <hr />
 
-      {data.description && <p className="lead">{data.description}</p>}
+        {data.description && <p className="lead">{data.description}</p>}
 
-      {data.homepage && (
-        <a href={data.homepage} target="_blank" rel="noopener noreferrer" className="no-underline font-semibold break-all">
-          <BsLink45Deg /> {data.homepage}
-        </a>
-      )}
+        {data.homepage && (
+          <a href={data.homepage} target="_blank" rel="noopener noreferrer" className="no-underline font-semibold break-all">
+            <BsLink45Deg /> {data.homepage}
+          </a>
+        )}
 
-      {!!data?.topics?.length && (
-        <p className="flex flex-row flex-wrap gap-2 mt-5 font-semibold text-xs">
-          {data.topics.map((item: string) =>
-            <a
-              key={item}
-              href={`https://github.com/topics/${item}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="no-underline py-1 px-2 rounded-xl bg-blue-200 bg-mode"
-            >
-              {item}
-            </a>
-          )}
-        </p>
-      )}
-      
-      {!!markdown && (
-        <MarkdownView className="my-5">
-          {markdown}
-        </MarkdownView>
-      )}
+        {!!data?.topics?.length && (
+          <p className="flex flex-row flex-wrap gap-2 mt-5 font-semibold text-xs">
+            {data.topics.map((item: string) =>
+              <a
+                key={item}
+                href={`https://github.com/topics/${item}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="no-underline py-1 px-2 rounded-xl bg-blue-200 bg-mode"
+              >
+                {item}
+              </a>
+            )}
+          </p>
+        )}
+        
+        {!!markdown && (
+          <MarkdownView className="my-5">
+            {markdown}
+          </MarkdownView>
+        )}
+      </article>
     </>
 }
---
+++ src/components/SpeechContent/index.tsx
@@ -0,0 +1,240 @@
+'use client';
+
+import { useState, useEffect, useRef } from 'react';
+import { useTheme } from 'next-themes';
+import Button from 'react-bootstrap/Button';
+import FormSelect from 'react-bootstrap/FormSelect';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { BsSpeedometer, BsMusicNoteBeamed, BsVolumeUpFill, BsVolumeMuteFill, BsMegaphone, BsStopFill, BsPauseFill } from "react-icons/bs";
+import { cx } from '@/utils';
+
+type SpeechContentProps = {
+  className?: string
+  text?: string
+  dropdownProps?: any
+}
+
+export const SpeechContent = ({
+  className,
+  text,
+  dropdownProps,
+}: SpeechContentProps) => {
+  const { theme } = useTheme();
+  const utteranceRef = useRef() as any;
+  const [voice, setVoice] = useState('0');
+  const [rateValue, setRateValue] = useState<number>(1);
+  const [pitchValue, setPitchValue] = useState(1);
+  const [volumeValue, setVolumeValue] = useState(1);
+  const [isSpeak, setIsSpeak] = useState(!1);
+
+  const speechSyn: any = typeof window !== 'undefined' && window.speechSynthesis;
+  const SpeechUtterance: any = typeof window !== 'undefined' && window.SpeechSynthesisUtterance;
+  const [supported, setSupported] = useState(false);
+  
+  useEffect(() => {
+    setSupported(!!speechSyn && !!SpeechUtterance);
+  }, []);
+
+  useEffect(() => {
+    const endSpeak = () => {
+      setIsSpeak(false);
+      // console.log(`Utterance has finished being spoken after ${e.elapsedTime} seconds.`);
+    }
+
+    const errorSpeak = (e: any) => {
+      if(e.error !== 'interrupted'){
+        setIsSpeak(false);
+      }
+      console.log('speech synthesis error: ' + e.error); // 'An error has occurred with the speech synthesis: ' + e.error
+    }
+
+    const beforeUnload = () => {
+      speechSyn.cancel(); // Stop
+    }
+
+    if(utteranceRef.current){
+      utteranceRef.current.addEventListener("error", errorSpeak);
+      utteranceRef.current.addEventListener("end", endSpeak);
+      window.addEventListener("beforeunload", beforeUnload, { capture: true });
+    }
+    
+    return () => {
+      utteranceRef.current?.removeEventListener?.("error", errorSpeak);
+      utteranceRef.current?.removeEventListener?.("end", endSpeak);
+      window.removeEventListener("beforeunload", beforeUnload, { capture: true });
+    }
+  }, [utteranceRef.current]);
+
+  const voices = speechSyn.getVoices().map((item: any, i: number) => Object.assign(item, { value: '' + i }) );
+
+  const voiceOptions = voices
+    .map(({ value, name, lang }: any) => ({ value, name, lang }) )
+    .sort((a: any, b: any) => {
+      const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+      if ( aname < bname ) return -1;
+      else if ( aname == bname ) return 0;
+      else return +1;
+    });
+
+  const toggleSpeak = (value?: any) => {
+    if(text && window.SpeechSynthesisUtterance){
+      if(speechSyn.speaking && !speechSyn.paused){
+        speechSyn.pause(); // pause narration
+        setIsSpeak(false);
+        return;
+      }
+
+      setIsSpeak(true);
+
+      if(speechSyn.paused){
+        speechSyn.resume(); // unpause/resume narration
+        return;
+      }
+
+      utteranceRef.current = new SpeechUtterance(text);
+
+      utteranceRef.current.voice = voices[value || voice];
+      utteranceRef.current.rate = rateValue;
+      utteranceRef.current.pitch = pitchValue;
+      utteranceRef.current.volume = volumeValue;
+
+      speechSynthesis.speak(utteranceRef.current);
+    }
+  }
+
+  const stopSpeak = () => {
+    if(speechSyn.speaking){
+      speechSyn.cancel(); // stop narration
+      setIsSpeak(false);
+    }
+  }
+
+  const changeVoice = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const val = e.target.value;
+    setVoice(val);
+
+    if(isSpeak){
+      stopSpeak(); // Stop
+      toggleSpeak(val);
+    }
+  }
+
+  if(!supported){
+    return null;
+  }
+
+  return (
+    <div className={cx("flex gap-1", className)}>
+      <FormSelect
+        value={voice}
+        onChange={changeVoice}
+      >
+        {voiceOptions.map((item: any) =>
+          <option key={item.value} value={item.value}>{item.name} ({item.lang})</option>
+        )}
+      </FormSelect>
+
+      <Dropdown {...dropdownProps}>
+        <Dropdown.Toggle
+          variant={theme}
+          bsPrefix=" "
+          title="Speech rate"
+        >
+          <BsSpeedometer />
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="min-w-0 w-full shadow-md">
+          <div className="flex flex-col items-center">
+            <b>+</b>
+            <div className="range-v">
+              <input
+                className="form-range"
+                type="range"
+                min={0.5}
+                max={2}
+                step={0.1}
+                autoComplete="off"
+                value={rateValue}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setRateValue(+e.target.value)}
+              />
+            </div>
+            <b>-</b>
+          </div>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Dropdown {...dropdownProps}>
+        <Dropdown.Toggle
+          variant={theme}
+          bsPrefix=" "
+          title="Speech pitch"
+        >
+          <BsMusicNoteBeamed />
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="min-w-0 w-full shadow-md">
+          <div className="flex flex-col items-center">
+            <b>+</b>
+            <div className="range-v">
+              <input
+                className="form-range"
+                type="range"
+                min={0.5}
+                max={2}
+                step={0.1}
+                autoComplete="off"
+                value={pitchValue}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPitchValue(+e.target.value)}
+              />
+            </div>
+            <b>-</b>
+          </div>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Dropdown {...dropdownProps}>
+        <Dropdown.Toggle
+          disabled={isSpeak}
+          variant={theme}
+          bsPrefix=" "
+          title="Speech volume"
+        >
+          {volumeValue < 1 ? <BsVolumeMuteFill /> : <BsVolumeUpFill />}
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="min-w-0 w-full shadow-md">
+          <div className="flex flex-col items-center">
+            <b>+</b>
+            <div className="range-v">
+              <input
+                className="form-range"
+                type="range"
+                min={0}
+                max={1}
+                step={0.05}
+                autoComplete="off"
+                value={volumeValue}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVolumeValue(+e.target.value)}
+              />
+            </div>
+            <b>-</b>
+          </div>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Button
+        variant={theme}
+        title={isSpeak ? 'Pause' : 'Speak'}
+        onClick={() => toggleSpeak()}
+      >
+        {isSpeak ? <BsPauseFill /> : <BsMegaphone />}
+      </Button>
+      
+      <Button
+        variant={theme}
+        title="Stop speak"
+        disabled={!isSpeak}
+        onClick={stopSpeak}
+      >
+        <BsStopFill />
+      </Button>
+    </div>
+  )
+}
