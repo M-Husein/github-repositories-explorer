d41714f01784c8755996a6f3edfb915b73ea929b
M-Husein <m.husein27@gmail.com>
2023-06-25 20:01:48 +0700

    feat: add metadata in detail repo page

--- src/app/repository/[user]/[repo]/page.tsx
+++ src/app/repository/[user]/[repo]/page.tsx
@@ -1,19 +1,54 @@
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
 import { RepoDetail } from '@/components/RepoDetail';
 import { APP_NAME } from '@/const/APPS';
 
 type pageProps = {
-  params?: { user?: string, repo?: string }
+  params: { user?: string, repo?: string }
 }
 
-export const metadata: Metadata = {
-  title: "Detail repository",
-  openGraph: {
-    title: "Detail repository | " + APP_NAME,
-  },
-  twitter: {
-    title: "Detail repository | " + APP_NAME
-  },
+// export const metadata: Metadata = {
+//   title: "Detail repository",
+//   openGraph: {
+//     title: "Detail repository | " + APP_NAME,
+//   },
+//   twitter: {
+//     title: "Detail repository | " + APP_NAME
+//   },
+// }
+
+export async function generateMetadata(
+  { params }: pageProps,
+  // parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  // const id = params.id
+ 
+  // // fetch data
+  // const product = await fetch(`https://.../${id}`).then((res) => res.json())
+ 
+  // // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+ 
+  // return {
+  //   title: product.title,
+  //   openGraph: {
+  //     images: ['/some-specific-page-image.jpg', ...previousImages],
+  //   },
+  // }
+
+  const { user, repo } = params;
+  const title = user && repo ? user + '/' + repo : "Detail repository";
+  // const title = user + '/' + repo;
+
+  return {
+    title,
+    openGraph: {
+      title: title + " | " + APP_NAME,
+    },
+    twitter: {
+      title: title + " | " + APP_NAME
+    },
+  }
 }
 
 export default function Detail({ params }: pageProps){
